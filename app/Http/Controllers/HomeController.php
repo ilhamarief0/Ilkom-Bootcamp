6bf3e9cf2fe39f8f9b6d5efbe534ad86738bb5a9
ilhamariefprabowo <ilham@technosstudio.com>
2024-05-28 20:39:37 +0800

    Add Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,34 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-
-
-    public function tambah()
+    public function index()
     {
-        $c = 10;
-        $d = 20;
-
-        return $c + $d;
-    }
-    public function bagi()
-    {
-        $c = 10;
-        $d = 20;
-
-        return $c / $d;
-    }
-    public function kali()
-    {
-        $c = 10;
-        $d = 20;
-
-        return $c * $d;
-    }
-    public function kurang()
-    {
-        $c = 10;
-        $d = 20;
-
-        return $c - $d;
+        return view('dashboard.index');
     }
 }
